56d6dec5d8749b3e6b07247d389df278d520bafe
Jonathan Williams <jtw365@live.missouristate.edu>
2018-01-29 00:43:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ if (name === "Susan") {
 }
 
 var name = "Susan"
+
+if (name === "Susan") {
+    console.log("The name is correct")
+}
+
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
